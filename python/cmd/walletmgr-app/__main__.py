a02580bfeb14042bad504430f63dfb311e6ad181
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2024-01-22 04:53:50 +0000

    startup: add gtknodes [temporary]

--- python/cmd/walletmgr-app/__main__.py
+++ python/cmd/walletmgr-app/__main__.py
@@ -2,8 +2,9 @@ import sys
 
 import gi
 
-gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk
+gi.require_version("Gtk", "3.0")
+gi.require_version("GtkNodes", "0.1")
+from gi.repository import GLib, Gtk, GtkNodes
 
 class WalletManagerGUIApplication(Gtk.Application):
     def __init__(self):
